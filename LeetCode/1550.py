46104c339fec3f136523e830083cf0b5e068898c
HROZNY17 <ivansymochko2006@gmail.com>
2025-05-11 12:48:00 +0300

    LeetCode Problem 1550

---
+++ LeetCode/1550.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        n = len(arr)
+        for i in range((n -2)):
+            if arr[i] & arr[i +1] & arr[i  + 2] & 1:
+                return True
+        return False
