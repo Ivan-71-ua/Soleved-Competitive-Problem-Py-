bf726230a40dd9c64e19d8221809108cb9a26c2f
HROZNY17 <ivansymochko2006@gmail.com>
2025-11-14 10:54:54 +0200

    LeetCode Problem 2536 v1

---
+++ LeetCode/2536.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        mat = [[0] * n for _ in range(n)]
+
+        for r1, c1, r2, c2 in queries:
+            mat[r1][c1] += 1
+            if r2 + 1 < n:
+                mat[r2 + 1][c1] -= 1
+
+            if c2 + 1 < n:
+                mat[r1][c2 + 1] -= 1
+
+            if r2 + 1 < n and c2 + 1 < n:
+                mat[r2 + 1][c2 + 1] += 1
+
+        for i in range(n):
+            for j in range(1, n):
+                mat[i][j] += mat[i][j - 1]
+
+        for i in range(n):
+            for j in range(1, n):
+                mat[j][i] += mat[j - 1][i]
+
+        return mat
+
