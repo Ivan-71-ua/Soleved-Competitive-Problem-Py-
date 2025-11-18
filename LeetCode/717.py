a30996ddaa67f857dfc8b22474a588568523e156
HROZNY17 <ivansymochko2006@gmail.com>
2025-11-18 12:13:53 +0200

    LeetCode Problem 717 v1

---
+++ LeetCode/717.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n, i = len(bits), 0
+        while i < n - 1:
+            i += bits[i] + 1
+        return i == n - 1
