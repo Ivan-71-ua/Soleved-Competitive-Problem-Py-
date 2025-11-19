a20715edc6a3cfc4d1f3ed3f8af30b510ffec274
HROZNY17 <ivansymochko2006@gmail.com>
2025-11-19 10:43:53 +0200

    LeetCode Problem 2154 v1

---
+++ LeetCode/2154.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums.sort()
+        for n in nums:
+            if n == original:
+                original *= 2
+        return original
