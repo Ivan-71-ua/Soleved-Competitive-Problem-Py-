c8ed5496fb503285915647019d6037f3de444cac
HROZNY17 <ivansymochko2006@gmail.com>
2025-11-22 10:18:37 +0200

    LeetCode Problem 3190 v1

---
+++ LeetCode/3190.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        cnt = 0
+        for num in nums:
+            cnt += min(3 - num % 3, num % 3)
+        return cnt
