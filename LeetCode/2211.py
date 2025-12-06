5cf2476462745d2a91151226f7fd882dfa026ef7
HROZNY17 <ivansymochko2006@gmail.com>
2025-12-06 14:37:03 +0200

    LeetCode Problem 2211 v1.1

--- LeetCode/2211.py
+++ LeetCode/2211.py
@@ -1,16 +1,24 @@
 
+
 class Solution:
     def countCollisions(self, directions: str) -> int:
         cnt = 0
+        acum = 0
         road = []
         for direction in directions:
-            if road and road[-1] == "R" and direction == "L":
-                cnt += 2
-                road.append(direction)
-            if road and road[-1] == "S" and direction == "L":
-                road.append(direction)
+            if road and road[-1] == 'S' and direction == 'L':
                 cnt += 1
-            if direction != "L":
+            elif road and road[-1] == 'R' and direction == 'L':
+                road.append('S')
+                cnt += acum + 1
+                acum = 0
+            elif road and road[-1] == 'R' and direction == 'S':
+                cnt += acum
+                acum = 0
+                road.append('S')
+            elif direction != 'L':
+                if direction == 'R':
+                    acum += 1
                 road.append(direction)
 
         return cnt
