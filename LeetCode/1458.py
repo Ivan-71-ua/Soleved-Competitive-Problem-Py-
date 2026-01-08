00ba37b4b00a8e7bfb4d07d72a1ed956684d2a8c
HROZNY17 <ivansymochko2006@gmail.com>
2026-01-08 21:36:36 +0200

    LeetCode Problem 1458 v1

---
+++ LeetCode/1458.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        n, m = len(nums1), len(nums2)
+        dp = [[float('-inf')] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                take = nums1[i - 1] * nums2[j - 1] + max(0, dp[i - 1][j - 1])
+                dp[i][j] = max(take, dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n][m]
