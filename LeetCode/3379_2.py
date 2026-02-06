af36342e8581b3602fbc1aa203e18a2469c14629
HROZNY17 <ivansymochko2006@gmail.com>
2026-02-06 20:56:26 +0200

    LeetCode Problem 3379 v2

---
+++ LeetCode/3379_2.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        for i in range(n):
+            res[i] = nums[((i + nums[i]) % n + n) % n]
+        return res
